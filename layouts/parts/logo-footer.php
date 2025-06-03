4aa069a7fd6b6aa3542065c9fc873bc90845960d
Victor Ferreira <vitfera@hotmail.com>
2025-06-03 01:59:23 -0300

    adicionando logos ao rodapé

---
+++ layouts/parts/logo-footer.php
@@ -0,0 +1,4 @@
+<div class="logo-footer">
+    <img src="<?php $this->asset('img/logo-site.png'); ?>" alt="Prêmio Periferia Viva 2025">
+    <img src="<?php $this->asset('img/footer/ministerio-cidades.png'); ?>" alt="Ministério das Cidades">
+</div>
